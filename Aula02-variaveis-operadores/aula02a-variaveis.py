7786a25d9ffc1137240fe01697c9e31006d179e8
LucasMoraca <lucaskmoraca@gmail.com>
2026-03-19 22:28:45 -0300

    aula 2 variaveis e operadores

---
+++ Aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,32 @@
+print("Olá mundo!")
+
+print(7 + 4)
+
+print("7 + 4")
+
+print("7" + "4") #Concatenação de strings
+#comntários de 1 liha
+'''
+    Comentário
+    de multiplas linhas
+'''
+
+#Variáveis
+nome = "Lucas" # str
+idade = 19 # int
+peso = 80.2 # float
+
+print(nome, idade, peso)
+
+print(f"Oii, {nome}!!")
+
+
+#Inputs - simulação de forms no cmd
+nome = input("Digite o seu nome: ")
+
+idade = int(input("Digite sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
