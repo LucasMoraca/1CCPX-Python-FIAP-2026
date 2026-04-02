e0bdd5c8690bcaf9ddc9c7b5d89476de52bf8a58
LucasMoraca <lucaskmoraca@gmail.com>
2026-04-01 22:48:17 -0300

    Add files via upload

---
+++ Modelagem-Linear-Para-Aprendizado-de-Máquina/hello.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+# Utilizando o Python como Calculadora:
+
+# Soma
+soma = 5 + 3
+print(soma)
+print(5 + 3)
+
+# Subtração
+print(10 - 4)
+
+# Multiplicação
+print(10 * 3)
+
+# Divisão
+print(10 / 3)
+
+# Potenciação
+print(10 ** 3)
+
+# Radiciação
+print(144 ** (1/2))
