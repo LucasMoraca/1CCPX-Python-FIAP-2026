213b7efb6b5bb6a41ea2406a998d3a81a3f3e546
LucasMoraca <lucaskmoraca@gmail.com>
2026-04-09 22:24:54 -0300

    Add files via upload

---
+++ Aula03-Modulos/aula03a.py
@@ -0,0 +1,14 @@
+import math
+# from math import sqrt
+
+num = 4
+#raiz = sqrt(num)
+raiz = math.sqrt(num)
+print(f"A raíz de {num} é igual a {raiz:.2f}")
+
+import random
+num_rand = random.random()
+print(num_rand),
+
+mega_sena = random.randint(1, 60)
+print(mega_sena)
