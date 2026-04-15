e5f5af492d933a76695e79dc88c22fe6294f1b6e
LucasMoraca <lucaskmoraca@gmail.com>
2026-04-15 15:59:43 -0300

    Create exerc03.py

---
+++ Aula03-Modulos/exerc03.py
@@ -0,0 +1,10 @@
+num_1 = float(input("Digite o primeiro número: "))
+num_2 = float(input("Digite o segundo número: "))
+
+if (num_1 > num_2):
+  print(f"O maior número é {num_1}")
+elif (num_2 > num_1):
+  print(f"O maior número é {num_2}")
+else:
+  print("Os números são iguais")
+  
