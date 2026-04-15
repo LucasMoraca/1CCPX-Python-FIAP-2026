bd8cd2ad9ab234c18fdd42178777f7b32148b1bd
LucasMoraca <lucaskmoraca@gmail.com>
2026-04-15 15:58:36 -0300

    Create exerc02.py

---
+++ Aula03-Modulos/exerc02.py
@@ -0,0 +1,9 @@
+numero = float(input("Digite um número: "))
+
+# Verificando se o número é par
+# Se a divisão do numero por 2 tiver resto zero ele é par
+if numero % 2 == 0: 
+    print("O número é par.")
+# Se não ele é ímpar
+else: 
+    print("O número é impar.")
