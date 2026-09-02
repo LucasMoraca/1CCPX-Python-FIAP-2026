b4eec6eb2b72968128666176c7ff61d14be25ad7
LucasMoraca <lucaskmoraca@gmail.com>
2026-09-02 10:05:01 -0300

    Update desafio-email.py
    Apenas comentei melhor o código

--- Pensamento Computacional e Automação com Python/desafio-email.py
+++ Pensamento Computacional e Automação com Python/desafio-email.py
@@ -1,39 +1,46 @@
-entrada = input("Digite os e-mails separados por vírgula: ")
+# DESAFIO: ANALISADOR DE E-MAILS DA FIAP
 
-# Separa os e-mails e remove espaços extras
-emails = entrada.split(",")
+# Entrada dos dados
+entrada = input('Digite os e-mails separados por vírgula: ')
 
-usuarios = []
-dominios = {}
+# Separação dos e-mails
+emails = entrada.split(',')
 
-for email in emails:
-    email = email.strip()
+# Criação das estruturas
+usuarios = [] # Lista vazia
+dominios = {} # Dicionário vazio
 
-    # Separa usuário e domínio
-    usuario, dominio = email.split("@")
+for email in emails: # o for percorre cada item da lista emails
+    email = email.strip() # o método strip() remove os espaços no início e no final do texto
 
-    usuarios.append(usuario)
+    # Usando o método split('@') vamos dividir o e-mail em duas partes, sendo uma usuário e outra em domínio
+    usuario, dominio = email.split('@')
+
+    usuarios.append(usuario) # Adiciona o usuário na lista usuarios
 
     # Conta a quantidade de e-mails por domínio
-    if dominio not in dominios:
-        dominios[dominio] = 1
+    if dominio not in dominios:  # Verifica se o domínio já existe no dicionário.
+        dominios[dominio] = 1  # Caso o domínio ainda não exista Cria a chave e inicia a contagem em 1.
     else:
-        dominios[dominio] += 1
+        dominios[dominio] += 1  # Caso já exista Incrementa a quantidade.
 
 # Converte a lista de usuários em uma tupla
 usuarios = tuple(usuarios)
 
-print("\nRelatório:")
-print("Quantidade de e-mails por domínio:")
 
-for dominio, quantidade in dominios.items():
+# Cabeçalho
+print('\nRelatório')
+print('Quantidade de e-mails por domínio: ')
+
+# Percorre o dicionário
+for dominio, quantidade in dominios.items():  # O método items() devolve pares
     print(f"{dominio}: {quantidade}")
 
 print(f"\nLista de usuários: {usuarios}")
 
 # Exibe o primeiro e o último usuário
-print(f"Primeiro usuário: {usuarios[0]}")
-print(f"Último usuário: {usuarios[-1]}")
+print(f'Primeiro usuário: {usuarios[0]}')
+print(f'Último usuário: {usuarios[-1]}')
 
 # Como tuplas são imutáveis, criamos uma lista temporária
 usuarios_trocados = list(usuarios)
